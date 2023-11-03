8e377bcbb942bb9a2c5c04e4e6b99f39714ed522
Damián Pantoja <62729878+DeimianOne@users.noreply.github.com>
2023-11-03 05:00:13 -0300

    Deteccion de palabras ofensivas
    Se añadio una funcion para detectar palabras ofensivas al insertar un ticket. Las palabras son obtenidas desde la base de datos por lo que se creo un tabla en la base de datos para estas. De momento esto es un avance para el filtro de palabras ofensivas.

--- Proy TIS/crud-modular-login/pages/tickets/actions/store.php
+++ Proy TIS/crud-modular-login/pages/tickets/actions/store.php
@@ -2,14 +2,44 @@
     include("../../../database/connection.php");
     session_start();
 
+    function contieneMalasPalabras($texto, $conexion) {
+        $query = "SELECT palabra FROM palabra_ofensiva";
+        $resultConsulta = mysqli_query($conexion, $query);
+    
+        if ($resultConsulta) {
+            while ($row = $resultConsulta->fetch_assoc()) {
+                $palabra = $row['palabra'];
+                if (stripos($texto, $palabra) !== false) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     $rut_usuario = $_SESSION['rut_usuario'];
     $cod_departamento = $_POST["cod_departamento"];
     $tipo_solicitud = $_POST["tipo_solicitud"];
     $asunto_ticket = $_POST["asunto_ticket"];
     $detalles_solicitud = $_POST["detalles_solicitud"];
 
-    $query = "INSERT INTO ticket (cod_departamento, rut_usuario, tipo_solicitud, asunto_ticket, detalles_solicitud) VALUES ('$cod_departamento', '$rut_usuario', '$tipo_solicitud', '$asunto_ticket', '$detalles_solicitud')";
-    $result = mysqli_query($connection, $query);
+    // Verificar malas palabras
+    if (contieneMalasPalabras($detalles_solicitud, $connection)) {
+        $_SESSION['mensaje'] = "El texto contiene malas palabras. No se puede enviar el ticket.";
+    } else {
+        // Insertar el ticket en la base de datos
+        $query = "INSERT INTO ticket (cod_departamento, rut_usuario, tipo_solicitud, asunto_ticket, detalles_solicitud) VALUES ('$cod_departamento', '$rut_usuario', '$tipo_solicitud', '$asunto_ticket', '$detalles_solicitud')";
+        $resultInsercion = mysqli_query($connection, $query);
+
+        if ($resultInsercion) {
+            $_SESSION['mensaje'] = "Ticket enviado correctamente.";
+        } else {
+            $_SESSION['mensaje'] = "Error al enviar el ticket: " . mysqli_error($connection);
+        }
+    }
+
+    //$query = "INSERT INTO ticket (cod_departamento, rut_usuario, tipo_solicitud, asunto_ticket, detalles_solicitud) VALUES ('$cod_departamento', '$rut_usuario', '$tipo_solicitud', '$asunto_ticket', '$detalles_solicitud')";
+    //$result = mysqli_query($connection, $query);
 
     header("Location: ../../../index.php?p=tickets/index");
 ?>
